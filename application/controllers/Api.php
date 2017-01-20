9251659f2966dfdee93c45fe88371af6f3ea8898
ribvid <ribvid@gmail.com>
2017-01-20 23:04:04 +0100

    Popravljeni API-ji

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -53,9 +53,36 @@ class Api extends REST_Controller {
 
 	public function login_post() {
 		if ($this->ion_auth->login($this->post('username'), $this->post('password'))) {
-			$this->response(array('status' => 'success'));
+			$userId = $this->ion_auth->user()->row()->id;
+			$user = $this->Shop_model->get_user($userId);
+			$data = array(
+				"username"		=> $user["username"],
+				"email"			=> $user["email"],
+				"first_name"	=> $user["first_name"],
+				"last_name"		=> $user["last_name"],
+				"phone"			=> $user["phone"],
+				"street"		=> $user["street"],
+				"street_number"	=> $user["street_number"],
+				"city"			=> $user["city"],
+				"postcode"		=> $user["postcode"],
+				"country"		=> $user["country"],
+				"user_id"		=> $user["id"]
+			);
+			$this->response(array('status' => 'success', 'user' => $data));
 		} else {
-			$this->response(array('status' => 'failed'));
+			$users = $this->Shop_model->get_user();
+			$usernames = array_unique(array_map(function ($i) { return $i['username']; }, $users));
+
+			if (!$this->post('username')) {
+				$data = "Username is required.";
+			} else if (!$this->post('password')) {
+				$data = "Password is required.";
+			} else if (!in_array($this->post('username'), $usernames)) {
+				$data = "Username does not exist.";
+			} else {
+				$data = "Wrong password.";
+			}
+			$this->response(array('status' => 'failed', 'error' => $data));
 		}
 	}
 
@@ -65,6 +92,22 @@ class Api extends REST_Controller {
 		$identity = $this->post('username');
 		$password = $this->post('password');
 		$email = $this->post('email');
+
+		if (!$this->post('email')) {
+			$data = "Email is required.";
+		} else if (!$this->post('username')) {
+			$data = "Username is required.";
+		} else if (!$this->post('password')) {
+			$data = "Password is required.";
+		} else if (in_array($this->post('username'), $usernames)) {
+			$data = "This user already exists.";
+		}
+
+		if (isset($data)) {
+			$this->response(array('status' => 'failed', 'error' => $data)); 
+			return;
+		}
+
 		$additional_data = array(
 			'first_name' 	=> $this->post('first_name'),
 			'last_name'  	=> $this->post('last_name'),
@@ -75,10 +118,18 @@ class Api extends REST_Controller {
 			'country'		=> $this->post('country'),
 			'phone'      	=> $this->post('phone'),
 		);
+
 		if (!in_array($identity, $usernames) && $this->ion_auth->register($identity, $password, $email, $additional_data)) {
 			$this->response(array('status' => 'success'));
 		} else {
-			$this->response(array('status' => 'failed'));
+			if (!$this->post('first_name')) {
+				$data = "First name is required.";
+			} else if (!$this->post('last_name')) {
+				$data = "Last name is required.";
+			} else {
+				$data = "";
+			}
+			$this->response(array('status' => 'failed', 'error' => $data));
 		}
 	}
 
---
+++ application/controllers/MyProducts.php
@@ -0,0 +1,167 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class MyProducts extends CI_Controller {
+
+	public function __construct() {
+		parent::__construct();
+
+		// Določanje parametrov, katerim mora ustrezati naložena slika.
+		$config['upload_path'] = './uploads';
+		$config['allowed_types'] = 'gif|jpg|png|jpeg';
+		$config['max_size'] = 1000;
+		$config['max_width'] = 2048;
+		$config['max_height'] = 1536;
+
+		$this->load->model('Shop_model');
+		$this->load->helper('url');
+		$this->load->library('ion_auth');
+		$this->load->library('form_validation');
+		$this->load->library('upload', $config);
+	}
+
+	public function index() {
+		if (!$this->ion_auth->logged_in()) {
+			redirect('auth/login');
+		}
+		
+		$userId = $this->ion_auth->user()->row()->id;
+		$data["logged_in"] = $this->ion_auth->logged_in();
+		$data["user_group"] = $this->Shop_model->get_user_group($userId);
+		$data["products"] = $this->Shop_model->get_products($userId);
+
+		if ($data["user_group"]["id"] != 2) {
+			redirect('');
+		}
+
+		$this->load->view('templates/header');
+		$this->load->view('templates/nav', $data);
+		$this->load->view('my-products', $data);
+		$this->load->view('templates/shopping-cart', $data);
+		$this->load->view('templates/footer');
+	}
+
+	public function edit($id) {
+		if (!$this->ion_auth->logged_in()) {
+			redirect('auth/login');
+		}
+
+		$userId = $this->ion_auth->user()->row()->id;
+		$data["logged_in"] = $this->ion_auth->logged_in();
+		$data["user_group"] = $this->Shop_model->get_user_group($userId);
+		$data["product"] = $this->Shop_model->get_product($id);
+
+		if (!$data["product"] OR $data["user_group"]["id"] != 2 OR $userId != $data["product"]["users_id"]) {
+			redirect('');
+		}
+
+		$data["title"] = "Edit Product";
+		$data["btn"] = "Update product";
+		$data["action"] = "my-products/edit/".$id;
+		$data["photos"] = $this->Shop_model->get_product_gallery($id);
+
+		// Validacija poslanih podatkov
+		$this->form_validation->set_rules('name', 'product', 'required');
+		$this->form_validation->set_rules('description', 'description', 'required');
+		$this->form_validation->set_rules('price', 'price', 'numeric', 'required');
+
+		// Naloži preostale slike na strežnik
+		$this->upload->do_upload('image1');
+		$this->upload->do_upload('image2');
+		$this->upload->do_upload('image3');
+
+		// Če je pri nalaganju prišlo do napake (npr. slika ne ustreza podanim parametrom),
+		// sporoči uporabniku napako.
+		$data["upload_errors"] = "";
+		if ( $_FILES AND $_FILES['featuredImage']['name'] ) {
+			$data["upload_errors"] = ( ! $this->upload->do_upload('featuredImage') && $_SERVER['REQUEST_METHOD'] == 'POST' ) ? $this->upload->display_errors() : '';
+		}
+
+		// Če validacija ni uspela, to sporoči uporabniku, sicer dodaj
+		// izdelek v bazo in ga preusmeri na uvodno stran.
+		if ($this->form_validation->run() === FALSE OR ! empty($data["upload_errors"])) {
+
+			// Inputi
+			$data['name'] = array(
+				'type'	=> 'text', 
+				'class'	=> 'form-control',
+				'name'	=> 'name',
+				'value'	=> $data["product"]["name"]
+			);
+
+			$data['description'] = array(
+				'class'	=> 'form-control',
+				'rows'	=> 5,
+				'name'	=> 'description',
+				'value'	=> $data["product"]["description"]
+			);
+
+			$data['featuredImage'] = array(
+				'name'	=> 'featuredImage'
+			);
+
+			$data['price'] = array(
+				'type'	=> 'text', 
+				'class'	=> 'form-control',
+				'name'	=> 'price',
+				'value'	=> $data["product"]["price"]
+			);
+
+			$data['image1'] = array(
+				'name'	=> 'image1'
+			);
+
+			$data['image2'] = array(
+				'name'	=> 'image2'
+			);
+
+			$data['image3'] = array(
+				'name'	=> 'image3'
+			);
+
+			$this->load->view('templates/header');
+			$this->load->view('templates/nav', $data);
+			$this->load->view('new-product', $data);
+			$this->load->view('templates/shopping-cart', $data);
+			$this->load->view('templates/footer');
+		} else {
+			$this->Shop_model->update_product($userId, $data["product"]["id"]);
+			redirect('my-products', 'refresh');
+		}
+	}
+
+	public function deactivate($id) {
+		if (!$this->ion_auth->logged_in()) {
+			redirect('auth/login');
+		}
+
+		$userId = $this->ion_auth->user()->row()->id;
+		$user_group = $this->Shop_model->get_user_group($userId);
+		$product = $this->Shop_model->get_product($id);
+
+		if (!$product OR $user_group["id"] != 2 OR $userId != $product["users_id"]) {
+			redirect('');
+		}
+
+		$this->Shop_model->deactivate_product($product["id"]);
+		redirect('my-products', 'refresh');
+	}
+
+	public function activate($id) {
+		if (!$this->ion_auth->logged_in()) {
+			redirect('auth/login');
+		}
+
+		$userId = $this->ion_auth->user()->row()->id;
+		$user_group = $this->Shop_model->get_user_group($userId);
+		$product = $this->Shop_model->get_product($id);
+
+		if (!$product OR $user_group["id"] != 2 OR $userId != $product["users_id"]) {
+			redirect('');
+		}
+
+		$this->Shop_model->activate_product($product["id"]);
+		redirect('my-products', 'refresh');
+	}
+
+}
