e17ab6c046cb2ff69e56ed30e7da0fc66c85f80e
klavdij <klavdij.starman1@gmail.com>
2017-01-21 20:52:40 +0100

    only relative path to images, so you can access them from phone device.

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -27,9 +27,9 @@ class Api extends REST_Controller {
 		}
 
 		$product = $this->Shop_model->get_product( $this->get('id') );
-		$product["image"] = array(base_url().'uploads/'. $product["image"]);
+		$product["image"] = array('uploads/'. $product["image"]);
 		$images = $this->Shop_model->get_product_gallery($product["id"]);
-		foreach ($images as $image) array_push($product["image"], base_url().'uploads/'.$image["filename"]);
+		foreach ($images as $image) array_push($product["image"],'uploads/'.$image["filename"]);
 
 		// Sporoči, če produkt z navedenim ID-jem obstaja ali ne.
 		if ($product) {
@@ -43,9 +43,9 @@ class Api extends REST_Controller {
 		$products = $this->Shop_model->get_products();
 
 		for ($i = 0; $i < count($products); $i++) {
-			$products[$i]["image"] = array(base_url().'uploads/'. $products[$i]["image"]);
+			$products[$i]["image"] = array('uploads/'. $products[$i]["image"]);
 			$images = $this->Shop_model->get_product_gallery($products[$i]["id"]);
-			foreach ($images as $image) array_push($products[$i]["image"], base_url().'uploads/'.$image["filename"]);
+			foreach ($images as $image) array_push($products[$i]["image"],'uploads/'.$image["filename"]);
 		}
 
 		if ($products) {
