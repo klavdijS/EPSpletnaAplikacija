9b1cc284026caccc84bc0fe2f8c7ebe863a9751f
klavdij <klavdij.starman1@gmail.com>
2017-01-21 12:46:47 +0100

    added endpoint so user can change his data. need to fix the password changing option.

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -75,14 +75,18 @@ class Api extends REST_Controller {
 
 			if (!$this->post('username')) {
 				$data = "Username is required.";
+				$error_number = 1;
 			} else if (!$this->post('password')) {
 				$data = "Password is required.";
+				$error_number = 2;
 			} else if (!in_array($this->post('username'), $usernames)) {
 				$data = "Username does not exist.";
+				$error_number = 3;
 			} else {
 				$data = "Wrong password.";
+				$error_number = 4;
 			}
-			$this->response(array('status' => 'failed', 'error' => $data));
+			$this->response(array('status' => 'failed', 'error' => $data, 'error_number' => $error_number));
 		}
 	}
 
@@ -101,10 +105,11 @@ class Api extends REST_Controller {
 			$data = "Password is required.";
 		} else if (in_array($this->post('username'), $usernames)) {
 			$data = "This user already exists.";
+            $error_number = 4;
 		}
 
 		if (isset($data)) {
-			$this->response(array('status' => 'failed', 'error' => $data)); 
+			$this->response(array('status' => 'failed', 'error' => $data, 'error_number' => $error_number));
 			return;
 		}
 
@@ -132,5 +137,52 @@ class Api extends REST_Controller {
 			$this->response(array('status' => 'failed', 'error' => $data));
 		}
 	}
+
+	public function changeuser_post() {
+        if ($this->ion_auth->login($this->post('username'), $this->post('password'))) {
+            $userId = $this->ion_auth->user()->row()->id;
+            $this->Shop_model-> update_user($userId);
+            $updated = false;
+            if (isset($_POST['new_password'])) {
+                $data = array('password' => $this->post['new_password']);
+                $updated = $this -> ion_auth -> update($userId,$data);
+            }
+            $user = $this->Shop_model->get_user($userId);
+            $data = array(
+                "username"		=> $user["username"],
+                "email"			=> $user["email"],
+                "first_name"	=> $user["first_name"],
+                "last_name"		=> $user["last_name"],
+                "phone"			=> $user["phone"],
+                "street"		=> $user["street"],
+                "street_number"	=> $user["street_number"],
+                "city"			=> $user["city"],
+                "postcode"		=> $user["postcode"],
+                "country"		=> $user["country"],
+                "user_id"		=> $user["id"]
+            );
+            $this->response(array('status' => 'success', 'user' => $data,'password_updated' => $updated));
+
+        } else {
+            $users = $this->Shop_model->get_user();
+            $usernames = array_unique(array_map(function ($i) { return $i['username']; }, $users));
+
+            if (!$this->post('username')) {
+                $data = "Username is required.";
+                $error_number = 1;
+            } else if (!$this->post('password')) {
+                $data = "Password is required.";
+                $error_number = 2;
+            } else if (!in_array($this->post('username'), $usernames)) {
+                $data = "Username does not exist.";
+                $error_number = 3;
+            } else {
+                $data = "Wrong password.";
+                $error_number = 4;
+            }
+            $this->response(array('status' => 'failed', 'error' => $data, 'error_number' => $error_number));
+        }
+
+    }
 
 }
